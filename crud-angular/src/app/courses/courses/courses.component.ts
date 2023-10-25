fb6f587ff2b2550b4cc767818bd20cd09294c5a2
Camilo Almeida <camiloalmeida22@gmail.com>
2023-10-25 10:41:53 -0300

    created table
    Signed-off-by: Camilo Almeida <camiloalmeida22@gmail.com>

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Course} from "../models/course";
 
 @Component({
   selector: 'app-courses',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
+
+  courses: Course[] = [
+    {_id: '1', name: 'Angular', category: 'Front-End'}
+  ];
+  displayedColumns = ['name','category']
 
 }
