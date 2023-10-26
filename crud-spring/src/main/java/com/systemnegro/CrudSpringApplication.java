13c5c6d4bc9386c57d3554ca3302e8f5b54a0ba6
Camilo Almeida <camiloalmeida22@gmail.com>
2023-10-26 19:19:06 -0300

    api
    Signed-off-by: Camilo Almeida <camiloalmeida22@gmail.com>

--- crud-spring/src/main/java/com/systemnegro/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/systemnegro/CrudSpringApplication.java
@@ -1,13 +1,31 @@
 package com.systemnegro;
 
+import com.systemnegro.model.Course;
+import com.systemnegro.repository.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudSpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudSpringApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner initDatabase(CourseRepository courseRepository) {
+        return args -> {
+            courseRepository.deleteAll();
+
+            Course c = new Course();
+            c.setName("Angular com Spring");
+            c.setCategory("Front-End");
+
+            courseRepository.save(c);
+
+        };
+    }
 
 }
