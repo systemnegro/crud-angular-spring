2c60924d378a10d8cb19404496792814358cbc0a
Camilo Almeida <camiloalmeida22@gmail.com>
2023-12-14 16:02:52 -0300

    http post created
    Signed-off-by: Camilo Almeida <camiloalmeida22@gmail.com>

--- crud-spring/src/main/java/com/systemnegro/controller/CourseController.java
+++ crud-spring/src/main/java/com/systemnegro/controller/CourseController.java
@@ -3,10 +3,10 @@ package com.systemnegro.controller;
 import com.systemnegro.model.Course;
 import com.systemnegro.repository.CourseRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +20,10 @@ public class CourseController {
     public List<Course> list() {
         return courseRepository.findAll();
     }
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create (@RequestBody Course course){
+        return courseRepository.save(course);
+    }
 }
+
